03ff320f25fbf286e487da05592a5a88acdf1fb8
Bradley Buda <bradleybuda@gmail.com>
2013-03-18 00:30:34 -0700

    Change twitter widget to search, add live countdown

---
+++ js/countdown.js
@@ -0,0 +1,40 @@
+setInterval(function(){
+  var end = 1363676400;
+  var now = (new Date()).getTime() / 1000;
+  remaining = end - now
+
+  console.log(remaining);
+
+  if (remaining < 0) {
+    remaining = 0;
+  }
+
+  hours = Math.floor(remaining / (60 * 60));
+  minutes = Math.floor((remaining % (60 * 60)) / 60);
+  seconds = Math.floor(remaining % 60);
+
+  var s = "";
+
+  s += hours.toString();
+  if (hours == 1) {
+    s += " hour, ";
+  } else {
+    s += " hours, ";
+  }
+
+  s += minutes.toString();
+  if (minutes == 1) {
+    s += " minute, ";
+  } else {
+    s += " minutes, ";
+  }
+
+  s += seconds.toString();
+  if (seconds == 1) {
+    s += " second";
+  } else {
+    s += " seconds";
+  }
+
+  document.getElementById('time-remaining').innerHTML = s;
+}, 1000);
